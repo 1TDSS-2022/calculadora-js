ab49217810314a9f564662b0604986c0fe6f594e
vitoralmeidas <vitor.oliveira1904@gmail.com>
2022-05-04 21:04:45 -0300

    variaveis ok...

---
+++ script.js
@@ -0,0 +1,14 @@
+// para nao misturar classes de css com html, podemos usar outra opção
+// com o data-atributes e selecionar eles como se fosse querySelector
+// document.querySelectorAll('[data-number]') exemplo de como usar o data-atributes
+
+// variaveis: numbers e operations
+
+const numbersBtn = document.querySelectorAll("[data-number]");
+
+const operationsBtn = document.querySelectorAll("[data-operation]");
+const equalBtn = document.querySelector("[data-equals]");
+const delBtn = document.querySelector("[data-delete]");
+const allClearBtn = document.querySelector("[data-all-clear]");
+const previousText = document.querySelector("[data-previous]");
+const currentText = document.querySelector("[data-current]");
